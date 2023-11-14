463dbfca129d2b2814b4d728a76aef14cdc979e6
Chabs64 <chadhiggins6@gmail.com>
2023-11-14 17:06:27 -0500

    fixed python syntax and logical run step

--- game/Game.py
+++ game/Game.py
@@ -21,6 +21,7 @@ class Game(Engine):
         self.current_player = 1
         self.diceroled = False
         self.endturn = False
+        self.g = Gamestate.Gamestate()
 
         self.test_gui = SimpleGUI("Debug & Testing")
         self.money_slider = self.test_gui.add_slider("Money", 0, 1500, 0, 10)
@@ -45,6 +46,8 @@ class Game(Engine):
                                 callback=help.toggle_visibility)
 
     def tick_update(self, tick: Tick):
+
+
         super().tick_update(tick)
 
     def run(self):
@@ -60,91 +63,87 @@ class Game(Engine):
     #Logical Processing of actions returned from gamestate and calls to animations. Needs GUI calls to be completed
     def logicRun(self):
 
-        g = Gamestate
         #placeholder replace 0 with dice value
 
-        #while gamestatecon is true keep running the action loop
-        while g.Gamestate.getgamestatecon == True:
 
-            while(self.diceroled == False):
-                if(self.diceroled == True):
-                    dicevalue = 0 #add gui dice rolled method here
+        if(self.diceroled == True):
+            dicevalue = 0 #add gui dice rolled method here
 
-            self.diceroled == False
+        self.diceroled == False
 
-            action = g.Gamestate.gamelocation(dicevalue, self.current_player)
+        action = self.g.gamelocation(dicevalue, self.current_player)
 
-            if(action == "OfferToPayToLeaveJail"):
-                if(self.GUIpayjail() == True):
-                    g.Gamestate.leavejail(self.current_player)
-                    action = g.Gamestate.gamelocation(dicevalue, self.current_player)
+        if(action == "OfferToPayToLeaveJail"):
+            if(self.GUIpayjail() == True):
+                self.g.leavejail(self.current_player)
+                action = self.g.gamelocation(dicevalue, self.current_player)
 
-            if(action == "MoveToGo"):
-                self.PlacePlayer("GO")
+        if(action == "MoveToGo"):
+            self.PlacePlayer("GO")
 
-            elif (action == "OfferToBuyAirZandZRental"):
-                if (self.GUIpayjail() == True):
-                    g.Gamestate.BuyAirZandZRental(self.current_player)
-                self.PlacePlayer("AirZandZRental")
-
-            elif (action == "MoveToAirZandZRental"):
-                self.PlacePlayer("AirZandZRental")
-
-            elif (action == "MoveToJustVisiting"):
-                self.PlacePlayer("JustVisiting")
-
-            elif (action == "OfferToBuySuburbanTownHouse"):
-                if (self.GUIpayjail() == True):
-                    g.Gamestate.BuySuburbanTownHouse(self.current_player)
-                self.PlacePlayer("SuburbanTownHouse")
-
-            elif (action == "MoveToSuburbanTownHouse"):
-                self.PlacePlayer("SuburbanTownHouse")
-
-            elif (action == "OfferToBuyDownTownStudioApt"):
-                if (self.GUIpayjail() == True):
-                    g.Gamestate.BuyDownTownStudioApt(self.current_player)
-                self.PlacePlayer("DownTownStudioApt")
-
-            elif (action == "MoveToDownTownStudioApt"):
-                self.PlacePlayer("DownTownStudioApt")
-
-            elif (action == "MoveToCourtBattleThenJail"):
-                self.PlacePlayer("CourtBattle")
-                self.PlacePlayer("Jail")
-
-            elif (action == "OfferToBuySkyRiseFlat"):
-                if (self.GUIpayjail() == True):
-                    g.Gamestate.BuySkyRiseFlat(self.current_player)
-                self.PlacePlayer("SkyRiseFlat")
-
-            elif (action == "MoveToSkyRiseFlat"):
-                self.PlacePlayer("SkyRiseFlat")
-
-            elif (action == "EventAdd100"):
-                self.PlacePlayer("FreeParking")
-
-            elif (action == "EventPlus2x"):
-                self.PlacePlayer("FreeParking")
-
-            elif (action == "EventMinus2x"):
-                self.PlacePlayer("FreeParking")
+        elif (action == "OfferToBuyAirZandZRental"):
+            self.PlacePlayer("AirZandZRental")
+            if (self.GUIpayjail() == True):
+                self.g.BuyAirZandZRental(self.current_player)
 
 
-            waiting = True
-            while(waiting == True):
+        elif (action == "MoveToAirZandZRental"):
+            self.PlacePlayer("AirZandZRental")
 
-                if(self.endturn == True):
-                    endingAction = g.Gamestate.endturn()
-                    if(endingAction == "Next Players Turn"):
-                        self.playerturn()
-                        waiting = False
-                    elif(endingAction == "Player 1 Won"):
-                        waiting = False
-                    elif(endingAction == "Player 2 Won"):
-                        waiting = False
+        elif (action == "MoveToJustVisiting"):
+            self.PlacePlayer("JustVisiting")
 
-            self.endturn == False
+        elif (action == "OfferToBuySuburbanTownHouse"):
+            self.PlacePlayer("SuburbanTownHouse")
+            if (self.GUIpayjail() == True):
+                self.g.BuySuburbanTownHouse(self.current_player)
+
+
+        elif (action == "MoveToSuburbanTownHouse"):
+            self.PlacePlayer("SuburbanTownHouse")
+
+        elif (action == "OfferToBuyDownTownStudioApt"):
+            self.PlacePlayer("DownTownStudioApt")
+            if (self.GUIpayjail() == True):
+                self.g.BuyDownTownStudioApt(self.current_player)
+
+
+        elif (action == "MoveToDownTownStudioApt"):
+            self.PlacePlayer("DownTownStudioApt")
+
+        elif (action == "MoveToCourtBattleThenJail"):
+            self.PlacePlayer("CourtBattle")
+            self.PlacePlayer("Jail")
+
+        elif (action == "OfferToBuySkyRiseFlat"):
+            if (self.GUIpayjail() == True):
+                self.g.BuySkyRiseFlat(self.current_player)
+            self.PlacePlayer("SkyRiseFlat")
+
+        elif (action == "MoveToSkyRiseFlat"):
+            self.PlacePlayer("SkyRiseFlat")
+
+        elif (action == "EventAdd100"):
+            self.PlacePlayer("FreeParking")
+
+        elif (action == "EventPlus2x"):
+            self.PlacePlayer("FreeParking")
+
+        elif (action == "EventMinus2x"):
+            self.PlacePlayer("FreeParking")
+
+
+
+        if(self.endturn == True):
+            endingAction = self.g.endturn()
+            if(endingAction == "Next Players Turn"):
+                self.playerturn()
+
+            elif(endingAction == "Player 1 Won"):
+                ... #gui display winner
+            elif(endingAction == "Player 2 Won"):
+                ... #gui display winner
+        self.endturn == False
 
     #GUI Call for JAIL
     def GUIpayjail(self):
@@ -156,7 +155,7 @@ class Game(Engine):
     def PlacePlayer(self, Location: str):
 
         if(Location == "GO"):
-            x=1
+            x = 1
         if (Location == "AirZandZRental"):
             x = 1
         if (Location == "Jail"):
