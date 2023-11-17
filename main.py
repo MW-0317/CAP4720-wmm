407594b6323eb5bcd4ffdf7041cd9ee40b95a5c9
Mark Williams <mark.williams1703@gmail.com>
2023-11-17 15:18:13 -0500

    main

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from core.shaderLoader import ShaderProgram
 
 from game.Game import Game
 
+import math
+
 def main():
     width = 800
     height = 600
@@ -16,8 +18,13 @@ def main():
     shader = ShaderProgram("resources/shaders/object.glsl")
     textures = [Texture("./resources/images/Gameboard.png", "materialTexture")]
     o = Object("resources/objects/board.obj", shader, textures)
+    o2 = Object("resources/objects/cat.obj", shader, [])
     o.set_scale([2/(o.diameter)] * 3)
+    o2.set_scale([(2 / o2.diameter) / 3] * 3)
+    o2.set_position([100, 0, 0])
+    o2.set_rotation([math.pi / 2, 0, 0])
     s.add_object(o)
+    s.add_object(o2)
     s.add_object(c)
     g.add_scene(s)
     g.run()
